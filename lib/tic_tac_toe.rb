e3d04fba7edb0483f36e35dcf87e3c2b41d75d1a
Ruel Tingling <rvtingling@gmail.com>
2019-10-31 13:03:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,6 +48,140 @@ class TicTacToe
       false
     end
   end
+  def valid_move?(index)
+    if(@board[index] == " " && index>=0 && index<=8 )
+      true
+    else
+      false
+    end
+  end
+
+
+  def turn_count
+    @count = 0
+    @board.each { |e|
+      if e != " "
+        @count+=1
+      end
+    }
+    @count
+  end
+
+
+  def current_player
+    if(turn_count.even?)
+      "X"
+    else
+      "O"
+    end
+  end
+
+
+  def turn
+    input = gets.strip
+    @input_to_index = input_to_index(input)
+    @valid_move = valid_move?(@input_to_index)
+    @current_player = current_player
+
+
+    if(@valid_move)
+      move(@input_to_index,@current_player)
+      display_board
+    else
+      puts "invalid"
+      input = gets.strip
+    end
+  end
+
+
+  def won?
+    winning = []
+    WIN_COMBINATIONS.each_with_index { |combination,i|
+      winning[i] = []
+      combination.each{ |index|
+        if(@board[index] == "X") # Check if X is the winner
+          winning[i] << combination
+        else
+          winning[i] << false
+        end
+      }
+    }
+
+
+    winning = winning.find {|e| !e.include?(false)}
+    if(winning)
+      winning[0]
+    else # if_not, then it checks if O is the winner and show our final result
+      winning = []
+      WIN_COMBINATIONS.each_with_index { |combination,i|
+        winning[i] = []
+        combination.each{ |index|
+          if(@board[index] == "O")
+            winning[i] << combination
+          else
+            winning[i] << false
+          end
+        }
+      }
+      winning = winning.find {|e| !e.include?(false)}
+      if(winning)
+        winning[0]
+      else
+        winning
+      end
+    end
+  end
+
+
+  def full?
+    @board.all? { |e| e == "X" || e == "O" }
+  end
+
+
+  def draw?
+    if !won? && full?
+     return true
+   elsif !won? && !full?
+     return false
+   else won?
+     return false
+   end
+  end
+
+
+  def over?
+    if(draw? || won? || full?)
+      true
+    else
+      false
+    end
+  end
+
+
+  def winner
+    if(won?)
+      winning = @board[won?[0]]
+      if winning.include?("X")
+        "X"
+      elsif winning.include?("O")
+        "O"
+      end
+    else
+      nil
+    end
+  end
+
+
+  def play
+    until over?
+       turn
+    end
+    if won?
+       puts "Congratulations #{winner}!"
+    elsif draw?
+       puts "Cat's Game!"
+    end
+  end
 
   
   
