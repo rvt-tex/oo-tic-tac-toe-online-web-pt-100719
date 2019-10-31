4235b7e06b811c7a8800063a2273123b35e7a051
Ruel Tingling <rvtingling@gmail.com>
2019-10-31 13:08:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,9 +3,12 @@ class TicTacToe
   # array of possible win combinations 
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
   
+  attr_accessor :game 
+  
  #assign a instance variable @board to an array of 9 blank spaces " "
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+    @play = play
   end
  
 # defines the current player
